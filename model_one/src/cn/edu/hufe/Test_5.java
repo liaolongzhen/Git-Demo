00a09a1277f7d1dbb8cd780dd8e1fd896282f7dd
liaolongzhen <2074139047@qq.com>
2023-06-30 22:15:59 +0800

    test的test_5

---
+++ model_one/src/cn/edu/hufe/Test_5.java
@@ -0,0 +1,8 @@
+package cn.edu.hufe;
+
+public class Test_5 {
+    public static void main(String[] args) {
+        System.out.println("这是第二个java类");
+        System.out.println("20:45修改");
+    }
+}
